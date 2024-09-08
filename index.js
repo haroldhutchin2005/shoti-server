f7b4129989b634de560c929870eed5f425b18e85
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-09-08 12:00:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -303,7 +303,7 @@ async function generateVideo(userRank) {
   }
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
